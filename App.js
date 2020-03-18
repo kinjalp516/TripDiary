b6d67098b2cda31bbc6dee61286a3c40b1562683
Sam Minkin <samminkin@Sams-MBP.home>
2020-03-17 22:17:12 -0400

    Added components to App

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PhotosScreen from './views/PhotosScreen';
+import MapScreen from './views/MapScreen';
 
 import * as firebase from 'firebase';
 
@@ -24,7 +26,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Trip Diary</Text>
+        <PhotosScreen />
+        <MapScreen />
       </View>
     );
   }
