50c47a6b07d818f8b55e23f563557f58360fb038
Sam Minkin <samminkin@Sams-MBP.home>
2020-03-17 22:14:18 -0400

    Added view skeletons for Map and Photos

---
+++ views/MapScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default class MapScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Welcome to the map view!</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({});
---
+++ views/PhotosScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default class PhotosScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Welcome to the photos view!</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({});
