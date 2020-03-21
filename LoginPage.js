2296e53fdf9e7c0659e757ee166359ddf33e3923
Yash Shah <shahhyash@outlook.com>
2020-03-20 23:14:50 -0400

    Fixed Google Sign In Issue for Android

--- LoginPage.js
+++ LoginPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Button, StyleSheet } from 'react-native'
+
 import * as Google from 'expo-google-app-auth';
 import firebase from './Firebase.js';
 
@@ -15,14 +16,14 @@ export default class LoginPage extends React.Component {
     async signIn() {
         const config = {
             iosClientId: "640672912982-sjkd3s2luptsfrcm8ss344el2igp11ei.apps.googleusercontent.com",
-            androidClientId: "640672912982-d64pgql0ut8comuctu334htvjfc08et0.apps.googleusercontent.com",
+            androidClientId: "640672912982-e6d9dgbc2dja2pfpev2ogr34etsk6ois.apps.googleusercontent.com",
             iosStandaloneAppClientId: "640672912982-bpjtb8nbdt9jihjst4gebq9q30ai4p3b.apps.googleusercontent.com",
-            androidStandaloneAppClientId: "640672912982-d64pgql0ut8comuctu334htvjfc08et0.apps.googleusercontent.com"
+            androidStandaloneAppClientId: "640672912982-3ops35e5pkj4sgmgba79pvgdng4aomlk.apps.googleusercontent.com",
         };
-        const { type, accessToken, user } = await Google.logInAsync(config);
+        const { type, accessToken, idToken } = await Google.logInAsync(config);
         if (type === 'success') {
             const credential = 
-                firebase.auth.GoogleAuthProvider.credential(null, accessToken);
+                firebase.auth.GoogleAuthProvider.credential(idToken, accessToken);
             
             firebase.auth().signInWithCredential(credential).catch(
                 (error) => console.error("Error - signInWithCredential", error)
