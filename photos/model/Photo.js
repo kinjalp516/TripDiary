ae55f77139d6be6f71fc8e4a2ef2aef2c8a5ff55
Yash Shah <shahhyash@outlook.com>
2020-03-24 22:17:08 -0400

    Added Photo Model

---
+++ photos/model/Photo.js
@@ -0,0 +1,8 @@
+class Photo {
+    constructor(photoUrl, userId, location, dateTaken) {
+        this.photoUrl = photoUrl;
+        this.userId = userId;
+        this.location = location;
+        this.dateTaken = dateTaken;
+    }
+}
