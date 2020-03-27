d147ed8db9cee2e393eb7f58f1346d48b240f5e5
Sam <sminkin65@yahoo.com>
2020-03-26 20:41:15 -0400

    Fixed some styling issues

--- photos/PhotosPage.js
+++ photos/PhotosPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, View, Image, FlatList } from 'react-native';
 import { Appbar, Menu, Card, FAB, Text } from 'react-native-paper';
 
-import firebase from '../Firebase.js';
+import firebase from '../Firebase.js'; 
 import { Photo, fetchPhotos } from './model/Photo.js';
 
 export default class PhotosPage extends React.Component {
@@ -48,9 +48,78 @@ export default class PhotosPage extends React.Component {
       userId: 5,
       location: 'Hawaii',
       dateTaken: new Date(2020, 3, 23, 4, 50, 7, 14)
+    },
+    {
+      id: 6,
+      photoUrl: 'https://picsum.photos/703',
+      tripId: 6,
+      userId: 6,
+      location: 'Hawaii',
+      dateTaken: new Date(2020, 3, 23, 4, 50, 7, 8)
+    },
+    {
+      id: 7,
+      photoUrl: 'https://picsum.photos/704',
+      tripId: 7,
+      userId: 7,
+      location: 'Hawaii',
+      dateTaken: new Date(2020, 3, 23, 4, 51, 7, 14)
+    },
+    {
+      id: 8,
+      photoUrl: 'https://picsum.photos/705',
+      tripId: 8,
+      userId: 8,
+      location: 'Hawaii',
+      dateTaken: new Date(2020, 3, 23, 4, 46, 7, 14)
+    },
+    {
+      id: 9,
+      photoUrl: 'https://picsum.photos/706',
+      tripId: 9,
+      userId: 9,
+      location: 'Hawaii',
+      dateTaken: new Date(2020, 3, 23, 4, 32, 7, 14)
+    },
+    {
+      id: 10,
+      photoUrl: 'https://picsum.photos/707',
+      tripId: 10,
+      userId: 10,
+      location: 'Hawaii',
+      dateTaken: new Date(2020, 3, 23, 4, 50, 7, 45)
+    },
+    {
+      id: 11,
+      photoUrl: 'https://picsum.photos/708',
+      tripId: 11,
+      userId: 11,
+      location: 'Hawaii',
+      dateTaken: new Date(2020, 3, 23, 4, 50, 8, 14)
+    },
+    {
+      id: 12,
+      photoUrl: 'https://picsum.photos/709',
+      tripId: 12,
+      userId: 12,
+      location: 'Hawaii',
+      dateTaken: new Date(2020, 3, 23, 4, 50, 3, 14)
     }
   ]
   };
+
+  sortByDate() {
+    let arr = this.state.photos.slice();
+    arr.sort((a, b) => {
+      let dateOne = a.dateTaken, dateTwo = b.dateTaken;
+      if (dateOne.getHours() != dateTwo.getHours()) return dateOne.getHours() - dateTwo.getHours();
+      else if (dateOne.getMinutes() != dateTwo.getMinutes()) return dateOne.getMinutes() - dateTwo.getMinutes();
+      else if (dateOne.getSeconds() != dateTwo.getSeconds()) return dateOne.getSeconds() - dateTwo.getSeconds();
+      else if (dateOne.getMilliseconds() != dateOne.getMilliseconds()) dateOne.getMilliseconds() - dateTwo.getMilliseconds();
+      return 1;
+    });
+    this.setState({photos: arr})
+  }
 
   componentDidMount() {
     let user = firebase.auth().currentUser;
@@ -60,22 +129,6 @@ export default class PhotosPage extends React.Component {
     this.sortByDate();
 
     // use fetchPhotos to get all photos and store it in state
-  }
-
-  /**
-   * Sorts the array of photos by date before displaying them. Each photo was taken on the same day
-   * so we sort by hours, minutes, seconds, and then milliseconds.
-   */
-  sortByDate() {
-    let arr = this.state.photos.slice();
-    arr.sort((a, b) => {
-      let dateOne = a.dateTaken, dateTwo = b.dateTaken;
-      if (dateOne.getHours() != dateTwo.getHours()) return dateOne.getHours() - dateTwo.getHours();
-      else if (dateOne.getMinutes() != dateTwo.getMinutes()) return dateOne.getMinutes() - dateTwo.getMinutes();
-      else if (dateOne.getSeconds() != dateTwo.getSeconds()) return dateOne.getSeconds() - dateTwo.getSeconds();
-      return dateOne.getMilliseconds() - dateTwo.getMilliseconds();
-    });
-    this.setState({photos: arr})
   }
 
   render() {
@@ -92,7 +145,6 @@ export default class PhotosPage extends React.Component {
         <FlatList
           data={this.state.photos}
           renderItem={({item}) => {
-              console.log(item.photoUrl);
               return (
                 <View style={styles.photoComp}>
                   <Image style={styles.photo} source={{uri: item.photoUrl}} />
@@ -113,15 +165,10 @@ const styles = StyleSheet.create({
     flex: 1
   },
   photo: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    width: 195,
+    height: 150
   },
   photoComp: {
-    width: 150,
-    height: 100,
-    marginTop: 24,
-    marginLeft: 30,
-    marginRight: 24
-  }
+    padding: 5
+  },
 });
