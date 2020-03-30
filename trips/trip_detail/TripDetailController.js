6330b691c461afcef6cfb4da1c05cb6d28cc25be
vc372 <vc372@scarletmail.rutgers.edu>
2020-03-30 19:36:46 -0400

    quick fix

--- trips/trip_detail/TripDetailController.js
+++ trips/trip_detail/TripDetailController.js
@@ -21,7 +21,7 @@ export default class TripDetailController extends React.Component {
             { key: 'journal', title: 'Journal', icon: 'book'},
             { key: 'budget', title: 'Budget', icon: 'bank'},
             { key: 'map', title: 'Map', icon: 'map'},
-            { key: 'attraction', title: 'Attraction, icon:'}
+            { key: 'attraction', title: 'Attraction' , icon:''}
            // { key: 'daily', title: 'DailySummary', icon: 'view-day'}
         ]
     };
