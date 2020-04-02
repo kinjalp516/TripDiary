6c0dcf07c69a40641fb796f740c2c22b625b5b31
Nisha Bhat <nb599@scarletmail.rutgers.edu>
2020-04-02 17:14:38 -0400

    BudgetPage test

---
+++ __tests__/BudgetPage.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import BudgetPage from '../budget/view/BudgetPage';
+import { Trip } from '../trips/model/Trip';
+
+const trip = new Trip({
+    id: "",
+    name: "",
+    userId: "",
+    location: "",
+    startDate: new Date(2020, 3, 17),
+    endDate: new Date(2020, 3, 25)
+});
+
+
+test('Budget Page renders correctly', () =>{
+    
+        const tree = renderer.create(<BudgetPage trip={trip}  />).toJSON();
+        expect(tree).toMatchSnapshot();
+      });
