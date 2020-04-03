f021c7a0eaac226615d91e9e9650c7c4fa35dfbe
saz53 <60635932+saz53@users.noreply.github.com>
2020-04-03 14:07:14 -0400

    rename test

---
+++ __tests__/IntegrationTest1.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+
+import { Trip } from '../trips/model/Trip';
+import { Journal } from '../journal/model/Journal';
+import JournalPage from '../journal/view/JournalPage';
+import AddJournalPage from '../journal/view/AddJournalPage';
+
+//trip constant used for testing
+const trip = new Trip({
+    id: "",
+    name: "",
+    userId: "",
+    location: "",
+    startDate: new Date(2020, 3, 17),
+    endDate: new Date(2020, 3, 25)
+});
+
+//journal constant used for testing
+const journal = new Journal({
+    id: "",
+    userId: "",
+    title: "",
+    note: ""
+});
+
+test('Integration test: Checks journal entry made shows up correctly on Journal Page', () => {
+
+    const navigation = { navigate: jest.fn() };
+
+    //render add journal page and add journal entry
+    let tree = renderer.create(<AddJournalPage />).getInstance();
+    tree.state.title = 'Testing Title';
+    tree.state.note = 'Testing Note';
+
+    //pass the journal entry to the journal page from add journal page
+    let tree2 = renderer.create(<JournalPage />).getInstance();
+        tree2.state.journals[0] = new Journal({
+            id: "",
+        userId: "",
+        title: tree.state.title,
+        note: tree.state.note
+        });
+
+
+
+    //pass the journal entry to the journal page from add journal page
+
+    //test if journal on journal page is correct
+    expect(tree2.state.journals[0].title).toBe('Testing Title');
+    expect(tree2.state.journals[0].note).toBe('Testing Note');
+})
