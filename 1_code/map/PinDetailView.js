01bd05d11674bad52b596fa30f818cf7e83b687f
Sam <sminkin65@yahoo.com>
2020-04-22 18:21:24 -0400

    PinDetail Styling for sans photo

--- 1_code/map/PinDetailView.js
+++ 1_code/map/PinDetailView.js
@@ -9,7 +9,7 @@ export default class PinDetailView extends React.Component {
     // pass in a pin through props
     render() {
         return (
-            <Card style={styles.tripCard}>
+            <Card style={this.props.pin.photoUrl !== null ? styles.tripCardPhoto : styles.tripCard}>
                 <Card.Title title={this.props.pin.title} />
                 <Card.Content>
                     <Paragraph>{this.props.pin.description}</Paragraph>
@@ -23,12 +23,13 @@ export default class PinDetailView extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    tripCard: {
-        // marginTop: 24,
-        // marginLeft: 24,
-        // marginRight: 24,
+    tripCardPhoto: {
         width: 300,
         height: 280,
+    },
+    tripCard: {
+        width: 150,
+        height: 80,
     },
     photo: {
         padding: 10,
